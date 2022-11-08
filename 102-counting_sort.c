293926e82219a7317e5f9b651edff38ba25c0daa
Bezawork-pr <107026397+Bezawork-pr@users.noreply.github.com>
2022-11-08 12:59:17 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -38,7 +38,7 @@ int get_max_int(int *array, size_t size)
 void counting_sort(int *array, size_t size)
 {
 	size_t i, Maxnum = get_max_int(array, size);
-	int outputArray[100], index_array[450], sum = 0, new_index, index;
+	int outputArray[100], index_array[500], sum = 0, new_index, index;
 
 
 	if (size < 2 || array == NULL)
